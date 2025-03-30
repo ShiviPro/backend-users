6bdab0c8fe0a904c4bdc743c3545b51368d1a4e8
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-03-31 01:23:24 +0530

    chore: Add and configure .env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
@@ -57,7 +58,7 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
